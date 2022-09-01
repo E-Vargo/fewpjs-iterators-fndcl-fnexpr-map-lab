f56efd0cec086590c86da885fb2510b09c26bcc0
E-Vargo <vargoelliott@gmail.com>
2022-08-31 23:58:30 -0700

    Done.

--- index.js
+++ index.js
@@ -13,5 +13,13 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+  let words = tutorial.split(' ');
+  
+  let titleCased = words.map(word =>
+    word.charAt(0).toUpperCase() + word.slice(1)
+    );
+
+  return titleCased.join(' ');
+  });
+};
